4ba6e91b5cdf9f7ac328d6a3e9c042d7fdbad756
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2024-11-17 13:32:08 -0300

    animaçao

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -3,10 +3,11 @@ function showTextBox() {
   var textBox = document.getElementById("text-box");
   var click = document.getElementsByClassName("click")[0];
 
-  // Oculta o coração
+  // Oculta o botão de coração e o texto "click me"
   heartBtn.style.display = "none";
   click.style.display = "none";
 
-  // Exibe o retângulo com texto
+  // Exibe o retângulo com texto com animação
   textBox.style.display = "block";
+  textBox.classList.add("animate-text-box");
 }
