b11c64a0b4c5b8edb870fdc68a1f448b31bc90ce
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2024-11-22 17:29:54 -0300

    Atualizar o script.js

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -2,6 +2,7 @@ function showTextBox() {
   var heartBtn = document.getElementById("heart-btn");
   var textBox = document.getElementById("text-box");
   var click = document.getElementsByClassName("click")[0];
+  var supportBox = document.getElementById("support-box"); // Adicionando suporte ao novo box
 
   // Oculta o botão de coração e o texto "click me"
   heartBtn.style.display = "none";
@@ -10,4 +11,11 @@ function showTextBox() {
   // Exibe o retângulo com texto com animação
   textBox.style.display = "block";
   textBox.classList.add("animate-text-box");
+
+  // Exibe também o suporte box com animação
+  if (supportBox) {
+    supportBox.style.display = "block";
+    supportBox.classList.add("animate-text-box");
+  }
 }
+
